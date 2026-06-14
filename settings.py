4998cf2bbb19fba5a39da01b7ae2dc76b41be6fe
MartinChavez001 <martin.ezequiel.chave@gmail.com>
2026-06-13 22:28:54 -0300

    feat: migrate database from SQlite to PostgreSQL

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'manualpage'),
+        'USER': os.environ.get('POSTGRES_USER', 'manualpage'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'manualpage'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
