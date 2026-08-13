a75e03cef5b698f9bbbebec3bd0579fefb07f1bb
MartinChavez001 <martin.ezequiel.chave@gmail.com>
2026-08-13 19:58:28 -0300

    feat: add CSRF settings

--- settings.py
+++ settings.py
@@ -27,6 +27,8 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+
 USE_GENERATE_AVATARS = True
 
 
